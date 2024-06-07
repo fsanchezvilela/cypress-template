17004481516bed8711f402ad9f51440a253c6cf6
Fernando Sanchez Vilela <fernando.sanchez@servexternos.gruposantander.com>
2024-06-07 08:18:22 +0200

    fix: package json dependencies

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,30 +1,41 @@
 import { defineConfig } from "cypress";
 import { beforeRunHook, afterRunHook } from "cypress-mochawesome-reporter/lib";
 import cypressMochaAwesomeReporterPlugin from "cypress-mochawesome-reporter/plugin";
-import cypressWatchAndReloadPlugins from 'cypress-watch-and-reload/plugins'
-import cypressCodeCoveragePlugin from '@bahmutov/cypress-code-coverage/plugin'
-import cypressOnFix from 'cypress-on-fix';
-import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
-import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
-import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
+import cypressWatchAndReloadPlugins from "cypress-watch-and-reload/plugins";
+import cypressCodeCoveragePlugin from "@bahmutov/cypress-code-coverage/plugin";
+import cypressOnFix from "cypress-on-fix";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
 export default defineConfig({
   reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    outputFolder: "cypress/reports/html",
+    quiet: true,
+    overwrite: false,
+    html: {
+      filename: "index.html",
+    },
+    json: {
+      filename: "*.json",
+    },
+  },
   retries: 1,
   e2e: {
     async setupNodeEvents(cypressOn, config) {
-      const on = cypressOnFix(cypressOn)
+      const on = cypressOnFix(cypressOn);
       // implement node event listeners here
       cypressMochaAwesomeReporterPlugin(cypressOn);
 
       await addCucumberPreprocessorPlugin(on, config);
 
       // https://github.com/bahmutov/cypress-watch-and-reload
-      cypressWatchAndReloadPlugins(on, config)
+      cypressWatchAndReloadPlugins(on, config);
       // https://github.com/bahmutov/cypress-code-coverage
-      cypressCodeCoveragePlugin(on, config)
+      cypressCodeCoveragePlugin(on, config);
       on(
-        'file:preprocessor',
+        "file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin(config)],
         })
