90e23c43c0aeadef11c72fbbd3168bf91fb0e8d5
Fernando Sanchez Vilela <fernando.sanchez@servexternos.gruposantander.com>
2024-06-07 07:40:38 +0200

    add some plugins to handle reports with github action

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,34 @@
 import { defineConfig } from "cypress";
 import { beforeRunHook, afterRunHook } from "cypress-mochawesome-reporter/lib";
 import cypressMochaAwesomeReporterPlugin from "cypress-mochawesome-reporter/plugin";
+import cypressWatchAndReloadPlugins from 'cypress-watch-and-reload/plugins'
+import cypressCodeCoveragePlugin from '@bahmutov/cypress-code-coverage/plugin'
+import cypressOnFix from 'cypress-on-fix';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
 export default defineConfig({
   reporter: "cypress-mochawesome-reporter",
+  retries: 1,
   e2e: {
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(cypressOn, config) {
+      const on = cypressOnFix(cypressOn)
       // implement node event listeners here
-      cypressMochaAwesomeReporterPlugin(on);
+      cypressMochaAwesomeReporterPlugin(cypressOn);
 
+      await addCucumberPreprocessorPlugin(on, config);
+
+      // https://github.com/bahmutov/cypress-watch-and-reload
+      cypressWatchAndReloadPlugins(on, config)
+      // https://github.com/bahmutov/cypress-code-coverage
+      cypressCodeCoveragePlugin(on, config)
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
       // If you are override before:run or after:run hooks, use this:
       on("before:run", async (details) => {
         console.log("override before:run");
---
+++ cypress/support/step_definitions/index.ts
@@ -0,0 +1,2 @@
+// (optional, if your are using cypress-cucumber-preprocessor) Add to cypress/support/step_definitions/index.js
+import 'cypress-mochawesome-reporter/cucumberSupport';
