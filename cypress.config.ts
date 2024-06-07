4c3fcb41b3cbe4b118f52f3926b2dba6b234c4bc
Fernando Sanchez Vilela <fernando.sanchez@servexternos.gruposantander.com>
2024-06-07 07:24:35 +0200

    feat: add before and after run hooks override

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,26 +1,24 @@
 import { defineConfig } from "cypress";
-// import { beforeRunHook, afterRunHook } from 'cypress-mochawesome-reporter/lib'
-import cypressMochaAwesomeReporterPlugin from 'cypress-mochawesome-reporter/plugin'
+import { beforeRunHook, afterRunHook } from "cypress-mochawesome-reporter/lib";
+import cypressMochaAwesomeReporterPlugin from "cypress-mochawesome-reporter/plugin";
 
 export default defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       cypressMochaAwesomeReporterPlugin(on);
 
-      /*
-        If you are override before:run or after:run hooks, use this:
-          on('before:run', async (details) => {
-            console.log('override before:run');
-            await beforeRunHook(details);
-          });
+      // If you are override before:run or after:run hooks, use this:
+      on("before:run", async (details) => {
+        console.log("override before:run");
+        await beforeRunHook(details);
+      });
 
-          on('after:run', async () => {
-            console.log('override after:run');
-            await afterRunHook();
-          });
-      */
+      on("after:run", async () => {
+        console.log("override after:run");
+        await afterRunHook();
+      });
     },
   },
   projectId: "z9d1jw",
@@ -32,4 +30,3 @@ export default defineConfig({
   },
   video: true,
 });
-
